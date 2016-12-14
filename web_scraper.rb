14903fd173e586160f4aa5d6f3569dbe13a54d44
Jeffrey Chang <jeffreycj.chang@gmail.com>
2016-12-14 22:38:51 +0000

    Scraping gets all 5000+ links from IMBD

--- web_scraper.rb
+++ web_scraper.rb
@@ -5,8 +5,8 @@ require 'pry'
 require 'csv'
 
 all_pages_link = []
-initial_link = "http://www.imdb.com/search/title?genres=history&title_type=feature&sort=moviemeter,asc&view=advanced&page=1&ref_=adv_prv"
-link = "http://www.imdb.com/search/title?genres=history&title_type=feature&sort=moviemeter,asc&view=advanced&page=107&ref_=adv_prv"
+link = "http://www.imdb.com/search/title?genres=history&title_type=feature&sort=moviemeter,asc&view=advanced&page=1&ref_=adv_prv"
+# link = "http://www.imdb.com/search/title?genres=history&title_type=feature&sort=moviemeter,asc&view=advanced&page=107&ref_=adv_prv"
 
 # next_button = parse_page.css('a.lister-page-next.next-page')
 
@@ -20,37 +20,63 @@ root_url = "http://www.imdb.com/search/title"
 
 
 
-def scrape_ids_from_link(link)
-  page = HTTParty.get(link)
-  parse_page = Nokogiri::HTML(page)
+# def scrape_ids_from_link(link)
 
-  page_links = []
+#   page_links = []
 
-  parse_page.css('.lister-item-header').css('a').each do |film|
-    page_links << film.attributes["href"].value
-  end
-  return page_links
-end
+#   parse_page.css('.lister-item-header').css('a').each do |film|
+#     page_links << film.attributes["href"].value
+#   end
+#   return page_links
+# end
 
- all_pages_link += scrape_ids_from_link("http://www.imdb.com/search/title?genres=history&title_type=feature&sort=moviemeter,asc&view=advanced&page=1&ref_=adv_prv")
- # puts all_pages_link
+#  all_pages_link += scrape_ids_from_link("http://www.imdb.com/search/title?genres=history&title_type=feature&sort=moviemeter,asc&view=advanced&page=1&ref_=adv_prv")
+#  # puts all_pages_link
 
-def has_next?(link)
+# def has_next?(link)
+#   page = HTTParty.get(link)
+#   parse_page = Nokogiri::HTML(page)
+#   next_button = parse_page.css('a.lister-page-next.next-page')
+#   next_button[0]? true : false
+# end
+
+# def button_link(link)
+
+# end
+
+
+
+
+  # Pry.start(binding)
+count = 1
+begin
+
+
   page = HTTParty.get(link)
   parse_page = Nokogiri::HTML(page)
   next_button = parse_page.css('a.lister-page-next.next-page')
-  next_button[0]? true : false
-end
 
 
-  button_link = next_button[0].attributes["href"].value
-  link = root_url + button_link
+  parse_page.css('.lister-item-header').css('a').each do |film|
+    all_pages_link << film.attributes["href"].value
+  end
+
+  # scrape_ids_from_link(link)
+
+  if next_button[0]
+    button_link = next_button[0].attributes["href"].value
+    link = root_url + button_link
+  end
+
+  puts "Scraping page #{count}"
+  count += 1
+  puts all_pages_link.length
+
+end while next_button[0]
 
 
-  Pry.start(binding)
 
-
-next_button[0].attributes["href"].value
+# next_button[0].attributes["href"].value
 # do
 #   invoke scrape_ids_from_link with initial with link
 
