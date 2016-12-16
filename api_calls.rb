1c1e2e40d85141d9f5a303e62c309a3a5d445a3f
Jeffrey Chang <jeffreycj.chang@gmail.com>
2016-12-16 19:19:44 +0000

    Ran api calls and compiled information to be used for initial seed

--- api_calls.rb
+++ api_calls.rb
@@ -3,27 +3,39 @@ require 'nokogiri'
 require 'json'
 require 'pry'
 
-file = File.read("./links.json")
+exports = []
 
-data_hash = JSON.parse(file)
+file = File.read("./keywords_links.json")
+file2 = File.read("./summary.json")
+
+movies_with_keywords = JSON.parse(file)
+movies_with_summary = JSON.parse(file2)
 
 # puts data_hash[1]
 imdb_api_root = "https://www.omdbapi.com/?i="
 
-open('ryo_test.json', 'w') {|f|
-  f << HTTParty.get("https://www.omdbapi.com/?i=#{data_hash[10]}").to_json
-  f << HTTParty.get("https://www.omdbapi.com/?i=#{data_hash[11]}").to_json
-  f << HTTParty.get("https://www.omdbapi.com/?i=#{data_hash[2]}").to_json
-  f << HTTParty.get("https://www.omdbapi.com/?i=#{data_hash[3]}").to_json
-  f << HTTParty.get("https://www.omdbapi.com/?i=#{data_hash[4]}").to_json
-  f << HTTParty.get("https://www.omdbapi.com/?i=#{data_hash[5]}").to_json
-  f << HTTParty.get("https://www.omdbapi.com/?i=#{data_hash[6]}").to_json
-  f << HTTParty.get("https://www.omdbapi.com/?i=#{data_hash[7]}").to_json
-  f << HTTParty.get("https://www.omdbapi.com/?i=#{data_hash[8]}").to_json
-  f << HTTParty.get("https://www.omdbapi.com/?i=#{data_hash[9]}").to_json
 
+count = 0
+ids = movies_with_keywords.keys
+
+begin
+
+  movie_info = HTTParty.get(imdb_api_root + ids[count])
+  movie_info["keywords"] = movies_with_keywords[ids[count]]
+  movie_info["id"] = ids[count]
+  if movies_with_summary.has_key?(ids[count])
+    movie_info["summary_text"] = movies_with_summary[ids[count]]
+  end
+    exports << movie_info
+  # Pry.start(binding)
+  count += 1
+  puts count
+end while count < 300
+
+# pp exports
+open('api_movie_call.json', 'w') {|f|
+  f << exports.to_json
 }
-
 # link = imdb_api_root + data_hash[0]
 # link = imdb_api_root + data_hash[1]
 
--- length.rb
+++ length.rb
@@ -1,24 +1,28 @@
 # require ("./summary2.json")
 require 'json'
-file = File.read("./keywords_links.json")
+file = File.read("./api_movie_call.json")
 test1 = JSON.parse(file)
-test2 = {}
+# test2 = {}
+puts test1.length
+# test1.each do |x|
+#   puts x["Title"]
+# end
 # puts file[0]
-movie_ids = test1.keys
+# movie_ids = test1.keys
 
-count = 0
+# count = 0
 
-begin
-test2[movie_ids[count]] = test1[movie_ids[count]]
-# puts test1[movie_ids[count]]
+# begin
+# test2[movie_ids[count]] = test1[movie_ids[count]]
+# # puts test1[movie_ids[count]]
 
-if count == 1
-  break
-end
+# if count == 1
+#   break
+# end
 
-count += 1
-end while count < 5
+# count += 1
+# end while count < 5
 
-open('test.json', 'w') {|f|
-  f << test2.to_json
-}
+# open('test.json', 'w') {|f|
+#   f << test2.to_json
+# }
