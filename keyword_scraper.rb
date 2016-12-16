42d5d5f366bc062438668e3148efca51ea705bb8
Jeffrey Chang <jeffreycj.chang@gmail.com>
2016-12-16 18:49:52 +0000

    Finished scraping all the keywords from 3000+ websites

--- keyword_scraper.rb
+++ keyword_scraper.rb
@@ -8,37 +8,41 @@ exports = {}
 
 root_url = "http://www.imdb.com/title/"
 
-file = File.read("./links.json")
+file = File.read("./summary.json")
 movie_links = JSON.parse(file)
-# count = 1
+count = 1
 
-movie_links.each do |link|
-
+movie_links.keys.each do |link|
+  # puts link
   full_link = root_url + link
 
   page = HTTParty.get(full_link)
   parse_page = Nokogiri::HTML(page)
 
+  if parse_page.css("#titleStoryLine").css(".see-more").css("nobr")[0] != nil
+    keyword_extension = parse_page.css("#titleStoryLine").css(".see-more").css("nobr")[0].children[0].attributes["href"].value
+    keyword_link = "http://www.imdb.com" + keyword_extension
 
-  keyword_extension = parse_page.css("#titleStoryLine").css(".see-more").css("nobr")[0].children[0].attributes["href"].value
-  keyword_link = "http://www.imdb.com" + keyword_extension
-
-  keyword_page = HTTParty.get(keyword_link)
-  keyword_parse_page = Nokogiri::HTML(keyword_page)
+    keyword_page = HTTParty.get(keyword_link)
+    keyword_parse_page = Nokogiri::HTML(keyword_page)
 
 
-  keywords_on_page = keyword_parse_page.css(".sodatext")
+    keywords_on_page = keyword_parse_page.css(".sodatext")
 
-  keywords = []
+    keywords = []
 
-  keywords_on_page.each do |keyword|
-    keywords <<  keyword.css("a").children.text
+    keywords_on_page.each do |keyword|
+      keywords << keyword.css("a").children.text
+    end
+
+    if keywords.length > 2
+      exports[link] = keywords
+      puts "Scraping imdb id #{link}"
+    end
   end
-
-  if keywords.length > 2
-    exports[link] = keywords
-  end
-
+  # Pry.start(binding)
+  puts "Scraping page #{count}"
+  count += 1
 end
 
 open('keywords_links.json', 'w') {|f|
--- length.rb
+++ length.rb
@@ -1,4 +1,24 @@
+# require ("./summary2.json")
 require 'json'
-file = File.read("./summary2 .json")
+file = File.read("./keywords_links.json")
 test1 = JSON.parse(file)
-puts test1.length
+test2 = {}
+# puts file[0]
+movie_ids = test1.keys
+
+count = 0
+
+begin
+test2[movie_ids[count]] = test1[movie_ids[count]]
+# puts test1[movie_ids[count]]
+
+if count == 1
+  break
+end
+
+count += 1
+end while count < 5
+
+open('test.json', 'w') {|f|
+  f << test2.to_json
+}
