dd1054b1c4001ab06dc993f3cefb1d2213765d5f
Your name <jaocoelho93@gmail.com>
2016-12-16 18:48:46 +0000

    ran some api calls to provide Ryo with info

--- api_calls.rb
+++ api_calls.rb
@@ -11,8 +11,17 @@ data_hash = JSON.parse(file)
 imdb_api_root = "https://www.omdbapi.com/?i="
 
 open('ryo_test.json', 'w') {|f|
-  f << HTTParty.get("https://www.omdbapi.com/?i=#{data_hash[0]}").to_json
-  f << HTTParty.get("https://www.omdbapi.com/?i=#{data_hash[1]}").to_json
+  f << HTTParty.get("https://www.omdbapi.com/?i=#{data_hash[10]}").to_json
+  f << HTTParty.get("https://www.omdbapi.com/?i=#{data_hash[11]}").to_json
+  f << HTTParty.get("https://www.omdbapi.com/?i=#{data_hash[2]}").to_json
+  f << HTTParty.get("https://www.omdbapi.com/?i=#{data_hash[3]}").to_json
+  f << HTTParty.get("https://www.omdbapi.com/?i=#{data_hash[4]}").to_json
+  f << HTTParty.get("https://www.omdbapi.com/?i=#{data_hash[5]}").to_json
+  f << HTTParty.get("https://www.omdbapi.com/?i=#{data_hash[6]}").to_json
+  f << HTTParty.get("https://www.omdbapi.com/?i=#{data_hash[7]}").to_json
+  f << HTTParty.get("https://www.omdbapi.com/?i=#{data_hash[8]}").to_json
+  f << HTTParty.get("https://www.omdbapi.com/?i=#{data_hash[9]}").to_json
+
 }
 
 # link = imdb_api_root + data_hash[0]
